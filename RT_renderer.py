ae1cbc68e4c53451b13b775cb58ca244115fa920
Peeranut Mekwisit <peeranut.4498@gmail.com>
2026-03-03 23:13:38 +0700

    Implement multiprocessing for rendering rows and add renderScene function for 4K output

--- RT_renderer.py
+++ RT_renderer.py
@@ -1,64 +1,103 @@
-# renderer class
-
 import RT_utility as rtu
 import numpy as np
 from PIL import Image as im
 import math
 import RT_pbar
+import multiprocessing
+
 
 class Renderer():
 
     def __init__(self, cCamera, iIntegrator, sScene) -> None:
-
         self.camera = cCamera
         self.integrator = iIntegrator
         self.scene = sScene
-        pass
+
+    def _render_row(self, j):
+        row_data = []
+        for i in range(self.camera.img_width):
+
+            pixel_color = rtu.Color(0, 0, 0)
+
+            for spp in range(self.camera.samples_per_pixel):
+                generated_ray = self.camera.get_ray(i, j)
+                pixel_color = pixel_color + \
+                    self.integrator.compute_scattering(
+                        generated_ray,
+                        self.scene,
+                        self.camera.max_depth
+                    )
+
+            row_data.append(pixel_color)
+
+        return j, row_data
+
+    def _render_row_jittered(self, j):
+        row_data = []
+        sqrt_spp = int(math.sqrt(self.camera.samples_per_pixel))
+
+        for i in range(self.camera.img_width):
+
+            pixel_color = rtu.Color(0, 0, 0)
+
+            for s_j in range(sqrt_spp):
+                for s_i in range(sqrt_spp):
+                    generated_ray = self.camera.get_jittered_ray(
+                        i, j, s_i, s_j
+                    )
+                    pixel_color = pixel_color + \
+                        self.integrator.compute_scattering(
+                            generated_ray,
+                            self.scene,
+                            self.camera.max_depth
+                        )
+
+            row_data.append(pixel_color)
+
+        return j, row_data
 
     def render(self):
-        # gather lights to the light list
+
         self.scene.find_lights()
-        renderbar = RT_pbar.start_animated_marker(self.camera.img_height*self.camera.img_width)
-        k = 0
-                
-        for j in range(self.camera.img_height):
-            for i in range(self.camera.img_width):
 
-                pixel_color = rtu.Color(0,0,0)
-                # shoot multiple rays at random locations inside the pixel
-                for spp in range(self.camera.samples_per_pixel):
-                    generated_ray = self.camera.get_ray(i, j)
-                    pixel_color = pixel_color + self.integrator.compute_scattering(generated_ray, self.scene, self.camera.max_depth)
+        renderbar = RT_pbar.start_animated_marker(
+            self.camera.img_height * self.camera.img_width
+        )
 
-                self.camera.write_to_film(i, j, pixel_color)
-                renderbar.update(k)
-                k = k+1
+        with multiprocessing.Pool() as pool:
+
+            results = pool.imap_unordered(
+                self._render_row,
+                range(self.camera.img_height)
+            )
+
+            for j, row_data in results:
+                for i, pixel_color in enumerate(row_data):
+                    self.camera.write_to_film(i, j, pixel_color)
+                    renderbar.update(j * self.camera.img_width + i)
 
     def render_jittered(self):
-        # gather lights to the light list
+
         self.scene.find_lights()
-        renderbar = RT_pbar.start_animated_marker(self.camera.img_height*self.camera.img_width)
-        k = 0
-        sqrt_spp = int(math.sqrt(self.camera.samples_per_pixel))
-                
-        for j in range(self.camera.img_height):
-            for i in range(self.camera.img_width):
 
-                pixel_color = rtu.Color(0,0,0)
-                # shoot multiple rays at random locations inside the pixel
-                for s_j in range(sqrt_spp):
-                    for s_i in range(sqrt_spp):
+        renderbar = RT_pbar.start_animated_marker(
+            self.camera.img_height * self.camera.img_width
+        )
 
-                        generated_ray = self.camera.get_jittered_ray(i, j, s_i, s_j)
-                        pixel_color = pixel_color + self.integrator.compute_scattering(generated_ray, self.scene, self.camera.max_depth)
+        with multiprocessing.Pool() as pool:
 
-                self.camera.write_to_film(i, j, pixel_color)
-                renderbar.update(k)
-                k = k+1
-        
+            results = pool.imap_unordered(
+                self._render_row_jittered,
+                range(self.camera.img_height)
+            )
+
+            for j, row_data in results:
+                for i, pixel_color in enumerate(row_data):
+                    self.camera.write_to_film(i, j, pixel_color)
+                    renderbar.update(j * self.camera.img_width + i)
+
 
     def write_img2png(self, strPng_filename):
         png_film = self.camera.film * 255
         data = im.fromarray(png_film.astype(np.uint8))
         data.save(strPng_filename)
-
--- main.py
+++ main.py
@@ -89,10 +89,47 @@ def renderMoving():
 
     renderer = rtren.Renderer(main_camera, intg, world)
     renderer.render_jittered()
-    renderer.write_img2png('week10_moving_nojitter.png')    
+    renderer.write_img2png('week10_moving_nojitter.png') 
+
+def renderScene():
+    main_camera = rtc.Camera()
+    main_camera.aspect_ratio = 16.0/9.0
+    main_camera.img_width = 3840
+    main_camera.center = rtu.Vec3(0,0,0)
+    main_camera.samples_per_pixel = 100
+    main_camera.max_depth = 7
+    main_camera.vertical_fov = 60
+    main_camera.look_from = rtu.Vec3(-2, 2, 1)
+    main_camera.look_at = rtu.Vec3(0, 0, -1)
+    main_camera.vec_up = rtu.Vec3(0, 1, 0)
+
+    defocus_angle = 0.0
+    focus_distance = 8.0
+    main_camera.init_camera(defocus_angle, focus_distance)
+    # add objects to the scene
+
+    tex_checker_bw = rtt.CheckerTexture(0.32, rtu.Color(.2, .2, .2), rtu.Color(.9, .9, .9))
+
+    mat_tex_checker_bw = rtm.TextureColor(tex_checker_bw)
+
+    mat_blinn1 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.8), 0.5, 0.2, 8)
+    mat_blinn2 = rtm.Blinn(rtu.Color(0.4, 0.5, 0.4), 0.5, 0.6, 8)
+    mat_blinn3 = rtm.Blinn(rtu.Color(0.8, 0.5, 0.4), 0.5, 0.2, 8)
+
+
+    world = rts.Scene()
+    world.add_object(rto.Sphere(rtu.Vec3(   0,-100.5,-1),  100, mat_tex_checker_bw))
+    world.add_object(rto.Sphere(rtu.Vec3(-1.0,   0.0,-1),  0.5, mat_blinn1))    # left
+    world.add_object(rto.Sphere(rtu.Vec3(   0,   0.0,-1),  0.5, mat_blinn2))    # center
+    world.add_object(rto.Sphere(rtu.Vec3( 1.0,   0.0,-1),  0.5, mat_blinn3))    # right
+
+    intg = rti.Integrator(bSkyBG=True)
+
+    renderer = rtren.Renderer(main_camera, intg, world)
+    renderer.render_jittered()
+    renderer.write_img2png('week10_testing_4K.png')    
+
 
 if __name__ == "__main__":
-    # renderDoF()
-    renderMoving()
-
+    renderScene()
 
